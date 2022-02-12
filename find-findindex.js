46c8963a4c1d366ea856d48fc862b8d1b59e919a
Ivy <ivy@onprem.com>
2022-02-11 23:44:24 -0600

    completed assignment

--- find-findindex.js
+++ find-findindex.js
@@ -11,8 +11,11 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername (usersArray, username) {
+	return usersArray.find(function (obj) {
+		return obj['username'] === username
+	})
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +30,13 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser (usersArray, username) {
+	let spliceIdx = usersArray.findIndex(function (obj) {
+		return obj['username'] === username
+	})
+	if (spliceIdx !== -1) {
+		return usersArray.splice(spliceIdx, 1)[0]
+		// Does the [0] just remove the array brackets when returning?
+	}
+	return undefined
+}
